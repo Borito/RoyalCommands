337f28c5559c9805e71e8b8da53cfd469ed35568
Kyle Clemens <jkc.clemens@gmail.com>
2011-12-09 19:14:29 -0500

    Fixed /compareip with no args

--- src/tk/royalcraf/royalcommands/RoyalCommandsCommandExecutor.java
+++ src/tk/royalcraf/royalcommands/RoyalCommandsCommandExecutor.java
@@ -573,40 +573,45 @@ public class RoyalCommandsCommandExecutor implements CommandExecutor {
 							+ "/getip and /compareip have been disabled.");
 					return true;
 				} else {
-					OfflinePlayer player1 = null;
-					OfflinePlayer player2 = null;
-					player1 = (OfflinePlayer) plugin.getServer()
-							.getOfflinePlayer(args[0]);
-					player2 = (OfflinePlayer) plugin.getServer()
-							.getOfflinePlayer(args[1]);
+					if (args.length < 2) {
+						return false;
+					} else {
+						OfflinePlayer player1 = null;
+						OfflinePlayer player2 = null;
+						player1 = (OfflinePlayer) plugin.getServer()
+								.getOfflinePlayer(args[0]);
+						player2 = (OfflinePlayer) plugin.getServer()
+								.getOfflinePlayer(args[1]);
 
-					File p1confl = new File(plugin.getDataFolder()
-							+ "/userdata/" + player1.getName() + ".yml");
-					File p2confl = new File(plugin.getDataFolder()
-							+ "/userdata/" + player2.getName() + ".yml");
-					if (p1confl.exists()) {
-						if (p2confl.exists()) {
-							FileConfiguration p1conf = YamlConfiguration
-									.loadConfiguration(p1confl);
-							FileConfiguration p2conf = YamlConfiguration
-									.loadConfiguration(p2confl);
-							String p1ip = p1conf.getString("ip");
-							String p2ip = p2conf.getString("ip");
+						File p1confl = new File(plugin.getDataFolder()
+								+ "/userdata/" + player1.getName() + ".yml");
+						File p2confl = new File(plugin.getDataFolder()
+								+ "/userdata/" + player2.getName() + ".yml");
+						if (p1confl.exists()) {
+							if (p2confl.exists()) {
+								FileConfiguration p1conf = YamlConfiguration
+										.loadConfiguration(p1confl);
+								FileConfiguration p2conf = YamlConfiguration
+										.loadConfiguration(p2confl);
+								String p1ip = p1conf.getString("ip");
+								String p2ip = p2conf.getString("ip");
 
-							sender.sendMessage(ChatColor.GRAY
-									+ player1.getName() + ": " + p1ip);
-							sender.sendMessage(ChatColor.GRAY
-									+ player2.getName() + ": " + p2ip);
-							return true;
+								sender.sendMessage(ChatColor.GRAY
+										+ player1.getName() + ": " + p1ip);
+								sender.sendMessage(ChatColor.GRAY
+										+ player2.getName() + ": " + p2ip);
+								return true;
+							} else {
+								sender.sendMessage(ChatColor.RED
+										+ "The player " + player2.getName()
+										+ " does not exist.");
+								return true;
+							}
 						} else {
 							sender.sendMessage(ChatColor.RED + "The player "
-									+ player2.getName() + " does not exist.");
+									+ player1.getName() + " does not exist.");
 							return true;
 						}
-					} else {
-						sender.sendMessage(ChatColor.RED + "The player "
-								+ player1.getName() + " does not exist.");
-						return true;
 					}
 				}
 			}
