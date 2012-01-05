145317440ad3baa5a8217c521e589f03f21fa8f0
Kyle Clemens <jkc.clemens@gmail.com>
2012-01-04 19:30:50 -0500

    Fixed: Small color issue in facepalm

--- src/tk/royalcraf/rcommands/Facepalm.java
+++ src/tk/royalcraf/rcommands/Facepalm.java
@@ -52,7 +52,8 @@ public class Facepalm implements CommandExecutor {
 											+ ChatColor.AQUA
 											+ " has facepalmed at "
 											+ ChatColor.YELLOW
-											+ victim.getName() + ".");
+											+ victim.getName() + ChatColor.AQUA
+											+ ".");
 							return true;
 						}
 					}
