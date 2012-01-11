f671c3d731fafe26cca41f77426dd50cd316bf17
Kyle Clemens <jkc.clemens@gmail.com>
2012-01-11 11:35:31 -0500

    Added: Support for /ohk

--- src/tk/royalcraf/royalcommands/RoyalCommandsEntityListener.java
+++ src/tk/royalcraf/royalcommands/RoyalCommandsEntityListener.java
@@ -1,7 +1,10 @@
 package tk.royalcraf.royalcommands;
 
 import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityListener;
 import org.bukkit.event.entity.EntityTargetEvent;
@@ -16,6 +19,21 @@ public class RoyalCommandsEntityListener extends EntityListener {
 	}
 
 	public void onEntityDamage(EntityDamageEvent event) {
+		if (event instanceof EntityDamageByEntityEvent) {
+			EntityDamageByEntityEvent ev = (EntityDamageByEntityEvent) event;
+			Entity e = ev.getDamager();
+			Entity ed = ev.getEntity();
+			if (e instanceof Player) {
+				Player p = (Player) e;
+				if (PConfManager.getPValBoolean((OfflinePlayer) p, "ohk")) {
+					if (ed instanceof LivingEntity) {
+						LivingEntity le = (LivingEntity) ed;
+						int leh = le.getMaxHealth();
+						le.damage(leh + 1);
+					}
+				}
+			}
+		}
 		if (event.getEntity() instanceof Player) {
 			Player p = (Player) event.getEntity();
 			if (PConfManager.getPValBoolean((OfflinePlayer) p, "godmode")) {
