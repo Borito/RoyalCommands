a718c79ea8ebffdbe06870c445e3d9770b93027f
Kyle Clemens <jkc.clemens@gmail.com>
2012-01-13 03:46:51 -0500

    Fixed: /spawner detects blocks much better now

--- src/tk/royalcraf/rcommands/Spawner.java
+++ src/tk/royalcraf/rcommands/Spawner.java
@@ -1,6 +1,7 @@
 package tk.royalcraf.rcommands;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.CreatureSpawner;
 import org.bukkit.command.Command;
@@ -8,8 +9,10 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.CreatureType;
 import org.bukkit.entity.Player;
-import org.bukkit.util.BlockIterator;
 import tk.royalcraf.royalcommands.RoyalCommands;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class Spawner implements CommandExecutor {
 
@@ -19,7 +22,56 @@ public class Spawner implements CommandExecutor {
         this.plugin = instance;
     }
 
+    // Borrowed list of materials from Essentials
+    private static final Set<Integer> AIR_MATERIALS = new HashSet<Integer>();
+    private static final HashSet<Byte> AIR_MATERIALS_TARGET = new HashSet<Byte>();
 
+    static
+    {
+        AIR_MATERIALS.add(Material.AIR.getId());
+        AIR_MATERIALS.add(Material.SAPLING.getId());
+        AIR_MATERIALS.add(Material.POWERED_RAIL.getId());
+        AIR_MATERIALS.add(Material.DETECTOR_RAIL.getId());
+        AIR_MATERIALS.add(Material.LONG_GRASS.getId());
+        AIR_MATERIALS.add(Material.DEAD_BUSH.getId());
+        AIR_MATERIALS.add(Material.YELLOW_FLOWER.getId());
+        AIR_MATERIALS.add(Material.RED_ROSE.getId());
+        AIR_MATERIALS.add(Material.BROWN_MUSHROOM.getId());
+        AIR_MATERIALS.add(Material.RED_MUSHROOM.getId());
+        AIR_MATERIALS.add(Material.TORCH.getId());
+        AIR_MATERIALS.add(Material.REDSTONE_WIRE.getId());
+        AIR_MATERIALS.add(Material.SEEDS.getId());
+        AIR_MATERIALS.add(Material.SIGN_POST.getId());
+        AIR_MATERIALS.add(Material.WOODEN_DOOR.getId());
+        AIR_MATERIALS.add(Material.LADDER.getId());
+        AIR_MATERIALS.add(Material.RAILS.getId());
+        AIR_MATERIALS.add(Material.WALL_SIGN.getId());
+        AIR_MATERIALS.add(Material.LEVER.getId());
+        AIR_MATERIALS.add(Material.STONE_PLATE.getId());
+        AIR_MATERIALS.add(Material.IRON_DOOR_BLOCK.getId());
+        AIR_MATERIALS.add(Material.WOOD_PLATE.getId());
+        AIR_MATERIALS.add(Material.REDSTONE_TORCH_OFF.getId());
+        AIR_MATERIALS.add(Material.REDSTONE_TORCH_ON.getId());
+        AIR_MATERIALS.add(Material.STONE_BUTTON.getId());
+        AIR_MATERIALS.add(Material.SUGAR_CANE_BLOCK.getId());
+        AIR_MATERIALS.add(Material.DIODE_BLOCK_OFF.getId());
+        AIR_MATERIALS.add(Material.DIODE_BLOCK_ON.getId());
+        AIR_MATERIALS.add(Material.TRAP_DOOR.getId());
+        AIR_MATERIALS.add(Material.PUMPKIN_STEM.getId());
+        AIR_MATERIALS.add(Material.MELON_STEM.getId());
+        AIR_MATERIALS.add(Material.VINE.getId());
+        AIR_MATERIALS.add(Material.NETHER_WARTS.getId());
+        AIR_MATERIALS.add(Material.WATER_LILY.getId());
+        AIR_MATERIALS.add(Material.SNOW.getId());
+
+        for (Integer integer : AIR_MATERIALS)
+        {
+            AIR_MATERIALS_TARGET.add(integer.byteValue());
+        }
+        AIR_MATERIALS_TARGET.add((byte)Material.WATER.getId());
+        AIR_MATERIALS_TARGET.add((byte)Material.STATIONARY_WATER.getId());
+    }
+    
     @Override
     public boolean onCommand(CommandSender cs, Command cmd, String label, String[] args) {
         if (cmd.getName().equalsIgnoreCase("spawner")) {
@@ -39,18 +91,7 @@ public class Spawner implements CommandExecutor {
                 return false;
             }
             Player p = (Player) cs;
-            BlockIterator b = new BlockIterator(p, 0);
-            if (!b.hasNext()) {
-                cs.sendMessage(ChatColor.RED + "No block there!");
-                return true;
-            }
-            Block bb = b.next();
-            while (b.hasNext()) {
-                if (b.next().getTypeId() != 0) {
-                    bb = b.next();
-                    break;
-                }
-            }
+            Block bb = p.getTargetBlock(AIR_MATERIALS_TARGET, 300);
             if (bb == null) {
                 cs.sendMessage(ChatColor.RED + "No block found!");
                 return true;
