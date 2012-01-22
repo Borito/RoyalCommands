3f83b8e8dc0815ed8966af661c2d596f23590f2e
Kyle Clemens <jkc.clemens@gmail.com>
2012-01-21 22:40:13 -0500

    Fixed: /sci on yourself

--- src/org/royaldev/rcommands/Sci.java
+++ src/org/royaldev/rcommands/Sci.java
@@ -29,59 +29,65 @@ public class Sci implements CommandExecutor {
                         + " was denied access to the command!");
                 return true;
             }
-            int errord = 0;
             if (args.length < 2) {
                 cs.sendMessage(cmd.getDescription());
                 return false;
             }
-            if (errord == 0) {
-                int removeID;
-                victim = plugin.getServer().getPlayer(args[0]);
-                if (plugin.isVanished(victim)) {
-                    cs.sendMessage(ChatColor.RED
-                            + "That player does not exist!");
-                    return true;
-                }
-                if (victim == null) {
-                    cs.sendMessage(ChatColor.RED
-                            + "You must input an online player.");
-                    errord = 1;
-                }
-                try {
-                    removeID = Integer.parseInt(args[1]);
-                } catch (NumberFormatException e) {
-                    cs.sendMessage(ChatColor.RED
-                            + "You must input a numerical ID to remove.");
-                    return false;
-                }
-                if (removeID <= 0 || removeID > 2266) {
-                    cs.sendMessage(ChatColor.RED
-                            + "You must specify a valid item ID.");
-                    return true;
-                }
-                if (removeID < 2255 && removeID > 382) {
-                    cs.sendMessage(ChatColor.RED
-                            + "You must specify a valid item ID.");
-                    return true;
-                }
-                if (plugin.isAuthorized(victim, "rcmds.exempt.sci")) {
+            int removeID;
+            victim = plugin.getServer().getPlayer(args[0]);
+            if (plugin.isVanished(victim)) {
+                cs.sendMessage(ChatColor.RED
+                        + "That player does not exist!");
+                return true;
+            }
+            if (victim == null) {
+                cs.sendMessage(ChatColor.RED
+                        + "You must input an online player.");
+                return true;
+            }
+            try {
+                removeID = Integer.parseInt(args[1]);
+            } catch (NumberFormatException e) {
+                cs.sendMessage(ChatColor.RED
+                        + "You must input a numerical ID to remove.");
+                return false;
+            }
+            if (removeID <= 0 || removeID > 2266) {
+                cs.sendMessage(ChatColor.RED
+                        + "You must specify a valid item ID.");
+                return true;
+            }
+            if (removeID < 2255 && removeID > 382) {
+                cs.sendMessage(ChatColor.RED
+                        + "You must specify a valid item ID.");
+                return true;
+            }
+            if (plugin.isAuthorized(victim, "rcmds.exempt.sci")) {
+                if (cs instanceof Player) {
+                    Player p = (Player) cs;
+                    if (p != victim) {
+                        cs.sendMessage(ChatColor.RED
+                                + "You cannot alter that player's inventory.");
+                        return true;
+                    }
+                } else {
                     cs.sendMessage(ChatColor.RED
                             + "You cannot alter that player's inventory.");
                     return true;
                 }
-                victim.getInventory().remove(removeID);
-                victim.sendMessage(ChatColor.RED
-                        + "You have just had all of your item ID "
-                        + ChatColor.BLUE + removeID + ChatColor.RED
-                        + " removed by " + ChatColor.RED + cs.getName()
-                        + ChatColor.BLUE + "!");
-                cs.sendMessage(ChatColor.BLUE
-                        + "You have just removed all of the item ID "
-                        + ChatColor.RED + removeID + ChatColor.BLUE + " from "
-                        + ChatColor.RED + victim.getName() + ChatColor.BLUE
-                        + "'s inventory.");
-                return true;
             }
+            victim.getInventory().remove(removeID);
+            victim.sendMessage(ChatColor.RED
+                    + "You have just had all of your item ID "
+                    + ChatColor.BLUE + removeID + ChatColor.RED
+                    + " removed by " + ChatColor.RED + cs.getName()
+                    + ChatColor.BLUE + "!");
+            cs.sendMessage(ChatColor.BLUE
+                    + "You have just removed all of the item ID "
+                    + ChatColor.RED + removeID + ChatColor.BLUE + " from "
+                    + ChatColor.RED + victim.getName() + ChatColor.BLUE
+                    + "'s inventory.");
+            return true;
         }
         return false;
     }
