7e140b85e6a3239d26a20a0de825446be84f2ae7
Kyle Clemens <jkc.clemens@gmail.com>
2012-01-23 03:34:35 -0500

    Added: /kill

---
+++ src/org/royaldev/rcommands/CmdKill.java
@@ -0,0 +1,40 @@
+package org.royaldev.rcommands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.royaldev.royalcommands.RUtils;
+import org.royaldev.royalcommands.RoyalCommands;
+
+public class CmdKill implements CommandExecutor {
+
+    RoyalCommands plugin;
+
+    public CmdKill(RoyalCommands instance) {
+        this.plugin = instance;
+    }
+
+
+    @Override
+    public boolean onCommand(CommandSender cs, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("kill")) {
+            if (!plugin.isAuthorized(cs, "rcmds.kill")) {
+                RUtils.dispNoPerms(cs);
+            }
+            if (args.length < 1) {
+                cs.sendMessage(cmd.getDescription());
+                return false;
+            }
+            Player t = plugin.getServer().getPlayer(args[0].trim());
+            if (t == null || plugin.isVanished(t)) {
+                cs.sendMessage(ChatColor.RED + "That player does not exist!");
+                return true;
+            }
+            t.setHealth(0);
+            cs.sendMessage(ChatColor.BLUE + "You have killed " + ChatColor.GRAY + t.getDisplayName() + ChatColor.BLUE + ".");
+        }
+        return false;
+    }
+}
