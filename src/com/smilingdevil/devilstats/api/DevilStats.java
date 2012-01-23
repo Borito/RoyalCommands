5b2ed5bed6d5166050f3a4479489d6b1f51b9f31
Kyle Clemens <jkc.clemens@gmail.com>
2012-01-23 02:06:26 -0500

    Added: DevilStats

--- src/com/smilingdevil/devilstats/api/DevilStats.java
+++ src/com/smilingdevil/devilstats/api/DevilStats.java
@@ -1,54 +1,88 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.smilingdevil.devilstats.api;
+
+import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author devil
  */
+@SuppressWarnings("unused")
 public class DevilStats {
-    public class SendPluginInformation extends Exception {
-        public SendPluginInformation(String ex) {
+    @SuppressWarnings("serial")
+    public class SendPluginInformationException extends Exception {
+        public SendPluginInformationException(String ex) {
             super(ex);
         }
     }
 
-    public enum ActionType {
-        STARTUP,
-        SHUTDOWN,
-        JUST_CHECKING
-    }
-
-    public enum DataType {
-        COUNT,
-        AUTHOR,
-        STATUS
-    }
-
-    private APIKey _key = null;
-
-    public DevilStats(APIKey key) {
-        this._key = key;
-    }
-
-    public String getData(DataType type) {
-        String action = null;
-        if (type.equals(DataType.COUNT)) {
-            action = "count";
-        } else if (type.equals(DataType.AUTHOR)) {
-            action = "author";
-        } else if (type.equals(DataType.STATUS)) {
-            action = "status";
+    @SuppressWarnings("serial")
+    public class NotMainClassException extends Exception {
+        public NotMainClassException() {
+            super("You are not accessing DevilStats from your main class. Please use your main class instead!");
         }
+    }
+
+    private String version = null;
+    private String name = null;
+    private String author = null;
+
+    private enum ActionType {
+        STARTUP, SHUTDOWN, JUST_CHECKING
+    }
+
+    private enum DataType {
+        COUNT, AUTHOR, STATUS
+    }
+
+    public DevilStats(JavaPlugin given) throws DevilStats.NotMainClassException {
+        if (given == null) {
+            throw new DevilStats.NotMainClassException();
+        } else {
+            name = given.getDescription().getName();
+            version = given.getDescription().getVersion();
+            ArrayList<String> authors = given.getDescription().getAuthors();
+            author = authors.get(0);
+            Logger log = Logger.getLogger("Minecraft");
+            log.log(Level.INFO, "[DevilStats] Successfully hooked into " + name + " version " + version);
+        }
+    }
+
+    public void sendStartup() {
+        sendData(DevilStats.ActionType.STARTUP);
+    }
+
+    public void sendShutdown() {
+        sendData(DevilStats.ActionType.SHUTDOWN);
+    }
+
+    public void checkConnection() {
+        sendData(DevilStats.ActionType.JUST_CHECKING);
+    }
+
+    public String getCount() {
+        return getData(DevilStats.DataType.COUNT);
+    }
+
+    public String getAuthor() {
+        return getData(DevilStats.DataType.AUTHOR);
+    }
+
+    public String getStatus() {
+        return getData(DevilStats.DataType.STATUS);
+    }
+
+    private String getData(DevilStats.DataType type) {
+        String action;
+        action = type.name().toLowerCase();
         try {
-            URL url = new URL("http://stats.smilingdevil.com/api?action=" + action + "&api_key="
-                    + _key.getValue());
+            URL url = new URL("http://stats.smilingdevil.com/api?action=" + action + "&version=" + version + "&author=" + author + "&name=" + name);
             URLConnection con = url.openConnection();
             return con.toString();
         } catch (Exception e) {
@@ -56,30 +90,28 @@ public class DevilStats {
         }
     }
 
-    public boolean sendData(ActionType type) {
+    private boolean sendData(DevilStats.ActionType type) {
         try {
             // Get action type
             String action;
-            if (type == ActionType.STARTUP) {
-                action = "startup";
-            } else if (type == ActionType.SHUTDOWN) {
-                action = "shutdown";
-            } else {
-                action = "checking";
-            }
+            action = type.name().toLowerCase();
             // Define URL for stats with action type
-            URL url = new URL("http://stats.smilingdevil.com/api?action=" + action + "&api_key=" + _key.getValue());
+            URL url = new URL("http://stats.smilingdevil.com/api?action="
+                    + action + "&version=" + version + "&author=" + author
+                    + "&name=" + name);
             // Open connection with server
             URLConnection con = url.openConnection();
             // Get contents of page
-            BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    url.openStream()));
             // Store contents of page
             String returned = in.readLine();
-            if (returned != null && returned.equals("SUCCESS")) { // Check if it's successful & not null
+            if (returned != null && returned.equals("SUCCESS")) {
                 return true; // We're all good
             } else {
                 // Something went wrong, wasn't successful / was null
-                throw new SendPluginInformation("An error occured while sending information: " + con);
+                throw new DevilStats.SendPluginInformationException(
+                        "An error occured while sending information: " + con);
             }
         } catch (Exception e) {
             return false;
