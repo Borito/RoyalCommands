e45c4db0cf743c5742abc14f50c4c3297feaab21
Kyle Clemens <jkc.clemens@gmail.com>
2012-01-25 12:41:23 -0500

    Updated: DevilStats

--- src/com/smilingdevil/devilstats/api/DevilStats.java
+++ src/com/smilingdevil/devilstats/api/DevilStats.java
@@ -1,25 +1,26 @@
 package com.smilingdevil.devilstats.api;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.bukkit.plugin.java.JavaPlugin;
 
 public class DevilStats {
 
     @SuppressWarnings("unused")
     private JavaPlugin plugin;
-    private HashMap<String, String> values = new HashMap<String, String>();
+    protected HashMap<String, String> values = new HashMap<String, String>();
 
     /**
-     *
      * @param plugin Reference to the parent class
-     * @throws Exception In the case that the parent class didn't give a value
-     * or a null value
+     * @throws Exception In the case that the parent class didn't give a value or a
+     *                   null value
      */
     public DevilStats(JavaPlugin plugin) throws Exception {
         if (plugin != null) {
+            // Valid plugin
             this.plugin = plugin;
             values.put("name", plugin.getDescription().getName());
             ArrayList<String> authors = plugin.getDescription().getAuthors();
@@ -30,14 +31,25 @@ public class DevilStats {
         }
     }
 
+    /**
+     * Logs a string to the console
+     *
+     * @param s String to log
+     */
     protected void log(String s) {
         Logger log = Logger.getLogger("Minecraft");
-        log.log(Level.INFO, "[DevilStats] "+s);
+        log.log(Level.INFO, "[DevilStats] " + s);
     }
 
+    /**
+     * Generate a URL for internal use only.
+     *
+     * @param action Action to input to the URL
+     * @return Returns the URL
+     */
     private String genURL(String action) {
         StringBuilder sb = new StringBuilder();
-        sb.append("http://stats.smilingdevil.com/api?").append("action=").append(action).append("&name=").append(values.get("name")).append("&version=").append(values.get("version")).append("&author=").append(values.get("author"));
+        sb.append("http://stats.smilingdevil.com/api?").append("action=").append(action).append("&plugin=").append(values.get("name")).append("&version=").append(values.get("version")).append("&author=").append(values.get("author"));
         return sb.toString();
     }
 
@@ -53,11 +65,15 @@ public class DevilStats {
         new Thread(thread).start();
     }
 
-    public String getPlugin() {
-        return values.get("name");
+    public void getCount() {
+        String url = genURL("get-count");
+        DevilThread thread = new DevilThread(url, this);
+        new Thread(thread).start();
     }
 
-    public String getVersion() {
-        return values.get("version");
+    public void getRank() {
+        String url = genURL("get-rank");
+        DevilThread thread = new DevilThread(url, this);
+        new Thread(thread).start();
     }
 }
--- src/com/smilingdevil/devilstats/api/DevilThread.java
+++ src/com/smilingdevil/devilstats/api/DevilThread.java
@@ -4,11 +4,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
- *
  * @author SmilingDevil
  */
 public class DevilThread implements Runnable {
@@ -27,12 +24,15 @@ public class DevilThread implements Runnable {
         try {
             URL url = new URL(_url);
             URLConnection conn = url.openConnection();
+            // Open input stream
             BufferedReader in = new BufferedReader(new InputStreamReader(
                     conn.getInputStream()));
             String inputLine;
 
             if ((inputLine = in.readLine()) != null)
+                // Handle result
                 parseResults(inputLine);
+            // Close
             in.close();
         } catch (Exception ex) {
             ex.printStackTrace();
@@ -41,10 +41,21 @@ public class DevilThread implements Runnable {
 
     private void parseResults(String result) {
         if (result.equals("SUCCESS")) {
-            hook.log("Successfully logged startup for " + hook.getPlugin()
-                    + " version " + hook.getVersion());
+            // Success
+            hook.log("Successfully logged startup for " + hook.values.get("name") + " version " + hook.values.get("version"));
+        } else if (result.equals("MISSING_INFO")) {
+            // We're missing something
+            hook.log("We seem to be missing some info from your plugin.yml!");
+            hook.log("DEBUG VALUES - ");
+            hook.log("Author: " + hook.values.get("author"));
+            hook.log("Plugin: " + hook.values.get("name"));
+            hook.log("Version: " + hook.values.get("version"));
+            hook.log("PLEASE report this log to SmilingDevil in #devilstats in irc.esper.net");
         } else {
-            System.out.println(result);
+            // How did you even get here?
+            // Other unhandled errors, possibly HTTP errors?
+            hook.log("DevilStats couldn't handle the returned value from the server.");
+            hook.log("Value: " + result);
         }
     }
 }
