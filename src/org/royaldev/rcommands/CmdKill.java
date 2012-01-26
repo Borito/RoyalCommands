2375dba96a7bc7e8982d428860a33a96ea329983
Kyle Clemens <jkc.clemens@gmail.com>
2012-01-25 20:04:29 -0500

    Fixed: rcmds.exempt.kill check

--- src/org/royaldev/rcommands/CmdKill.java
+++ src/org/royaldev/rcommands/CmdKill.java
@@ -33,7 +33,7 @@ public class CmdKill implements CommandExecutor {
                 cs.sendMessage(ChatColor.RED + "That player does not exist!");
                 return true;
             }
-            if (plugin.isAuthorized(cs, "rcmds.exempt.kill")) {
+            if (plugin.isAuthorized(t, "rcmds.exempt.kill")) {
                 cs.sendMessage(ChatColor.RED + "You cannot kill that player!");
                 return true;
             }
