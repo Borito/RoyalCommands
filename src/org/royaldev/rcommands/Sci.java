9eaaaaca71016c8614b992ee7dc820a570717ee2
Kyle Clemens <jkc.clemens@gmail.com>
2012-01-27 01:36:47 -0500

    Updated: Complete /sci rewrite

--- src/org/royaldev/rcommands/Sci.java
+++ src/org/royaldev/rcommands/Sci.java
@@ -1,10 +1,12 @@
 package org.royaldev.rcommands;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.royaldev.royalcommands.RUtils;
 import org.royaldev.royalcommands.RoyalCommands;
 
@@ -17,12 +19,8 @@ public class Sci implements CommandExecutor {
     }
 
     @Override
-    public boolean onCommand(CommandSender cs, Command cmd, String label,
-                             String[] args) {
+    public boolean onCommand(CommandSender cs, Command cmd, String label, String[] args) {
         if (cmd.getName().equalsIgnoreCase("sci")) {
-
-            Player victim;
-
             if (!plugin.isAuthorized(cs, "rcmds.sci")) {
                 RUtils.dispNoPerms(cs);
                 return true;
@@ -31,63 +29,174 @@ public class Sci implements CommandExecutor {
                 cs.sendMessage(cmd.getDescription());
                 return false;
             }
-            int removeID;
-            victim = plugin.getServer().getPlayer(args[0]);
-            if (plugin.isVanished(victim)) {
-                cs.sendMessage(ChatColor.RED
-                        + "That player does not exist!");
-                return true;
-            }
-            if (victim == null) {
-                cs.sendMessage(ChatColor.RED
-                        + "You must input an online player.");
-                return true;
-            }
-            try {
-                removeID = Integer.parseInt(args[1]);
-            } catch (NumberFormatException e) {
-                cs.sendMessage(ChatColor.RED
-                        + "You must input a numerical ID to remove.");
-                return false;
-            }
-            if (removeID <= 0 || removeID > 2266) {
-                cs.sendMessage(ChatColor.RED
-                        + "You must specify a valid item ID.");
-                return true;
-            }
-            if (removeID < 2255 && removeID > 382) {
-                cs.sendMessage(ChatColor.RED
-                        + "You must specify a valid item ID.");
-                return true;
-            }
-            if (plugin.isAuthorized(victim, "rcmds.exempt.sci")) {
-                if (cs instanceof Player) {
-                    Player p = (Player) cs;
-                    if (p != victim) {
-                        cs.sendMessage(ChatColor.RED
-                                + "You cannot alter that player's inventory.");
-                        return true;
-                    }
-                } else {
-                    cs.sendMessage(ChatColor.RED
-                            + "You cannot alter that player's inventory.");
+            if (args.length < 3) {
+                Player target = plugin.getServer().getPlayer(args[0].trim());
+                if (target == null || plugin.isVanished(target)) {
+                    cs.sendMessage(ChatColor.RED + "That player is not online!");
                     return true;
                 }
+                if (plugin.isAuthorized(target, "rcmds.exempt.sci")) {
+                    cs.sendMessage(ChatColor.RED + "You cannot alter that player's inventory!");
+                    return true;
+                }
+                String called = args[1];
+                String data = null;
+                if (called.contains(":")) {
+                    String[] calleds = called.split(":");
+                    called = calleds[0].trim();
+                    data = calleds[1].trim();
+                }
+                Integer iblock;
+                try {
+                    iblock = Integer.parseInt(called);
+                } catch (Exception e) {
+                    try {
+                        iblock = Material.getMaterial(called.trim().replace(" ", "_").toUpperCase()).getId();
+                    } catch (Exception e2) {
+                        cs.sendMessage(ChatColor.RED + "That block does not exist!");
+                        return true;
+                    }
+                }
+                if (iblock != 0) {
+                    ItemStack toInv;
+                    if (data != null) {
+                        if (Material.getMaterial(iblock) == null) {
+                            cs.sendMessage(ChatColor.RED + "Invalid item ID!");
+                            return true;
+                        }
+                        int data2;
+                        try {
+                            data2 = Integer.parseInt(data);
+                        } catch (Exception e) {
+                            cs.sendMessage(ChatColor.RED + "The metadata was invalid!");
+                            return true;
+                        }
+                        if (data2 < 0) {
+                            cs.sendMessage(ChatColor.RED + "The metadata was invalid!");
+                            return true;
+                        }
+                        toInv = new ItemStack(Material.getMaterial(iblock).getId(), plugin.defaultStack, (short) data2);
+                    } else {
+                        toInv = new ItemStack(Material.getMaterial(iblock).getId(), plugin.defaultStack);
+                    }
+                    target.getInventory().removeItem(toInv);
+                    cs.sendMessage(ChatColor.BLUE
+                            + "Removing "
+                            + ChatColor.GRAY
+                            + plugin.defaultStack
+                            + ChatColor.BLUE
+                            + " of "
+                            + ChatColor.GRAY
+                            + Material.getMaterial(iblock).toString()
+                            .toLowerCase().replace("_", " ")
+                            + ChatColor.BLUE + " from " + ChatColor.GRAY
+                            + target.getName() + ChatColor.BLUE + ".");
+                    target.sendMessage(ChatColor.BLUE
+                            + "You have had "
+                            + ChatColor.GRAY
+                            + plugin.defaultStack
+                            + ChatColor.BLUE
+                            + " of "
+                            + ChatColor.GRAY
+                            + Material.getMaterial(iblock).toString()
+                            .toLowerCase().replace("_", " ")
+                            + ChatColor.BLUE + " taken.");
+                    return true;
+                } else {
+                    cs.sendMessage(ChatColor.RED + "You cannot spawn air!");
+                    return true;
+                }
+            } else if (args.length == 3) {
+                Player target = plugin.getServer().getPlayer(args[0]);
+                if (target == null) {
+                    cs.sendMessage(ChatColor.RED + "That player is not online!");
+                    return true;
+                }
+                String called = args[1];
+                Integer amount;
+                String data = null;
+                if (called.contains(":")) {
+                    String[] calleds = called.split(":");
+                    called = calleds[0].trim();
+                    data = calleds[1].trim();
+                }
+                try {
+                    amount = Integer.parseInt(args[2]);
+                } catch (Exception e) {
+                    cs.sendMessage(ChatColor.RED
+                            + "The amount was not a number!");
+                    return true;
+                }
+                if (amount < 1) {
+                    amount = 1;
+                }
+                Integer iblock;
+                try {
+                    iblock = Integer.parseInt(called);
+                } catch (Exception e) {
+                    try {
+                        iblock = Material.getMaterial(called.trim().replace(" ", "_").toUpperCase()).getId();
+                    } catch (Exception e2) {
+                        cs.sendMessage(ChatColor.RED + "That block does not exist!");
+                        return true;
+                    }
+                }
+                if (iblock == 0) {
+                    cs.sendMessage(ChatColor.RED + "You cannot spawn air!");
+                    return true;
+                }
+                if (plugin.blockedItems.contains(iblock.toString()) && !plugin.isAuthorized(cs, "rcmds.allowed.item") && !plugin.isAuthorized(cs, "rcmds.allowed.item." + iblock.toString())) {
+                    cs.sendMessage(ChatColor.RED + "You are not allowed to spawn that item!");
+                    plugin.log.warning("[RoyalCommands] " + cs.getName() + " was denied access to the command!");
+                    return true;
+                }
+                ItemStack toInv;
+                if (data != null) {
+                    if (Material.getMaterial(iblock) == null) {
+                        cs.sendMessage(ChatColor.RED + "Invalid item ID!");
+                        return true;
+                    }
+                    int data2;
+                    try {
+                        data2 = Integer.parseInt(data);
+                    } catch (Exception e) {
+                        cs.sendMessage(ChatColor.RED + "The metadata was invalid!");
+                        return true;
+                    }
+                    if (data2 < 0) {
+                        cs.sendMessage(ChatColor.RED + "The metadata was invalid!");
+                        return true;
+                    } else {
+                        toInv = new ItemStack(Material.getMaterial(iblock).getId(), amount, (short) data2);
+                    }
+                } else {
+                    toInv = new ItemStack(Material.getMaterial(iblock).getId(), amount);
+                }
+                target.getInventory().removeItem(toInv);
+                cs.sendMessage(ChatColor.BLUE
+                        + "Removing "
+                        + ChatColor.GRAY
+                        + amount
+                        + ChatColor.BLUE
+                        + " of "
+                        + ChatColor.GRAY
+                        + Material.getMaterial(iblock).toString().toLowerCase()
+                        .replace("_", " ") + ChatColor.BLUE + " from "
+                        + ChatColor.GRAY + target.getName() + ChatColor.BLUE
+                        + ".");
+                target.sendMessage(ChatColor.BLUE
+                        + "You have had "
+                        + ChatColor.GRAY
+                        + amount
+                        + ChatColor.BLUE
+                        + " of "
+                        + ChatColor.GRAY
+                        + Material.getMaterial(iblock).toString().toLowerCase()
+                        .replace("_", " ") + ChatColor.BLUE + " taken.");
+                return true;
             }
-            victim.getInventory().remove(removeID);
-            victim.sendMessage(ChatColor.RED
-                    + "You have just had all of your item ID "
-                    + ChatColor.BLUE + removeID + ChatColor.RED
-                    + " removed by " + ChatColor.RED + cs.getName()
-                    + ChatColor.BLUE + "!");
-            cs.sendMessage(ChatColor.BLUE
-                    + "You have just removed all of the item ID "
-                    + ChatColor.RED + removeID + ChatColor.BLUE + " from "
-                    + ChatColor.RED + victim.getName() + ChatColor.BLUE
-                    + "'s inventory.");
-            return true;
         }
         return false;
     }
-
 }
+
