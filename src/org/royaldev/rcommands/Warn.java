87e9f4cbd1f098a4b8a02cf134de9c6bfe4c2ccf
Kyle Clemens <jkc.clemens@gmail.com>
2012-02-01 11:17:43 -0500

    Updated: /warn is cleaner

--- src/org/royaldev/rcommands/Warn.java
+++ src/org/royaldev/rcommands/Warn.java
@@ -38,98 +38,14 @@ public class Warn implements CommandExecutor {
             Player t = plugin.getServer().getPlayer(args[0].trim());
             if (t != null) {
                 if (plugin.isAuthorized(t, "rcmds.exempt.warn")) {
-                    cs.sendMessage(ChatColor.RED
-                            + "You cannot warn that player!");
+                    cs.sendMessage(ChatColor.RED + "You cannot warn that player!");
                     return true;
                 }
-                File pconfl = new File(plugin.getDataFolder() + "/userdata/"
-                        + t.getName().toLowerCase() + ".yml");
-                if (pconfl.exists()) {
-                    FileConfiguration pconf = YamlConfiguration
-                            .loadConfiguration(pconfl);
-                    Integer numwarns;
-                    String warnreason = null;
-                    if (pconf.get("warns") == null) {
-                        numwarns = 0;
-                    } else {
-                        numwarns = pconf.getConfigurationSection("warns")
-                                .getValues(false).size();
-                    }
-                    if (numwarns == null) {
-                        numwarns = 0;
-                    }
-                    if (args.length == 1) {
-                        warnreason = plugin.defaultWarn;
-                        pconf.set("warns." + (numwarns + 1), warnreason);
-                        try {
-                            pconf.save(pconfl);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    if (args.length > 1) {
-                        warnreason = plugin.getFinalArg(args, 1);
-                        pconf.set("warns." + (numwarns + 1), warnreason);
-                        try {
-                            pconf.save(pconfl);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    cs.sendMessage(ChatColor.BLUE + "You have warned "
-                            + ChatColor.GRAY + t.getName() + ChatColor.BLUE
-                            + ".");
-                    t.sendMessage(ChatColor.RED + "You have been warned by "
-                            + ChatColor.GRAY + cs.getName() + ChatColor.RED
-                            + " for " + ChatColor.GRAY + warnreason
-                            + ChatColor.RED + ".");
-                    plugin.getServer().broadcast(
-                            ChatColor.RED + "The player " + ChatColor.GRAY
-                                    + t.getName() + ChatColor.RED
-                                    + " has been warned for " + ChatColor.GRAY
-                                    + warnreason + ChatColor.RED + ".",
-                            "rcmds.see.warn");
-                    if (plugin.warnBan > 0) {
-                        if ((numwarns + 1) >= plugin.warnBan) {
-                            t.setBanned(true);
-                            t.kickPlayer(ChatColor.DARK_RED
-                                    + "You have been banned for reaching the max warn limit.");
-                            plugin.getServer()
-                                    .broadcast(
-                                            ChatColor.RED
-                                                    + "The player "
-                                                    + ChatColor.GRAY
-                                                    + t.getName()
-                                                    + ChatColor.RED
-                                                    + " has been banned for "
-                                                    + ChatColor.DARK_RED
-                                                    + "You have been banned for reaching the max warn limit."
-                                                    + ChatColor.RED + ".",
-                                            "rcmds.see.ban");
-                        }
-                    }
-                    return true;
-                } else {
+                File pconfl = new File(plugin.getDataFolder() + "/userdata/" + t.getName().toLowerCase() + ".yml");
+                if (!pconfl.exists()) {
                     cs.sendMessage(ChatColor.RED + "That user does not exist!");
                     return true;
                 }
-            }
-            OfflinePlayer t2 = plugin.getServer().getOfflinePlayer(
-                    args[0].trim());
-            if (t2.isOp()) {
-                cs.sendMessage(ChatColor.RED + "You cannot warn that player!");
-                return true;
-            }
-            if (t2.isOnline()) {
-                if (plugin.isAuthorized((Player) t2, "rcmds.exempt.warn")) {
-                    cs.sendMessage(ChatColor.RED
-                            + "You cannot warn that player!");
-                    return true;
-                }
-            }
-            File pconfl = new File(plugin.getDataFolder() + "/userdata/"
-                    + t2.getName().toLowerCase() + ".yml");
-            if (pconfl.exists()) {
                 FileConfiguration pconf = YamlConfiguration
                         .loadConfiguration(pconfl);
                 Integer numwarns;
@@ -137,8 +53,7 @@ public class Warn implements CommandExecutor {
                 if (pconf.get("warns") == null) {
                     numwarns = 0;
                 } else {
-                    numwarns = pconf.getConfigurationSection("warns")
-                            .getValues(false).size();
+                    numwarns = pconf.getConfigurationSection("warns").getValues(false).size();
                 }
                 if (numwarns == null) {
                     numwarns = 0;
@@ -161,42 +76,70 @@ public class Warn implements CommandExecutor {
                         e.printStackTrace();
                     }
                 }
-                cs.sendMessage(ChatColor.BLUE + "You have warned "
-                        + ChatColor.GRAY + t2.getName() + ChatColor.BLUE + ".");
-                if (t2.isOnline()) {
-                    ((Player) t2)
-                            .sendMessage(ChatColor.RED
-                                    + "You have been warned by "
-                                    + ChatColor.GRAY + cs.getName()
-                                    + ChatColor.RED + " for " + ChatColor.GRAY
-                                    + warnreason + ChatColor.RED + ".");
+                cs.sendMessage(ChatColor.BLUE + "You have warned " + ChatColor.GRAY + t.getName() + ChatColor.BLUE + ".");
+                t.sendMessage(ChatColor.RED + "You have been warned by " + ChatColor.GRAY + cs.getName() + ChatColor.RED + " for " + ChatColor.GRAY + warnreason + ChatColor.RED + ".");
+                plugin.getServer().broadcast(ChatColor.RED + "The player " + ChatColor.GRAY + t.getName() + ChatColor.RED + " has been warned for " + ChatColor.GRAY + warnreason + ChatColor.RED + ".", "rcmds.see.warn");
+                if (plugin.warnBan > 0) {
+                    if ((numwarns + 1) >= plugin.warnBan) {
+                        t.setBanned(true);
+                        t.kickPlayer(ChatColor.DARK_RED + "You have been banned for reaching the max warn limit.");
+                        plugin.getServer().broadcast(ChatColor.RED + "The player " + ChatColor.GRAY + t.getName() + ChatColor.RED + " has been banned for " + ChatColor.DARK_RED + "You have been banned for reaching the max warn limit." + ChatColor.RED + ".", "rcmds.see.ban");
+                    }
                 }
-                plugin.getServer().broadcast(
-                        ChatColor.RED + "The player " + ChatColor.GRAY
-                                + t2.getName() + ChatColor.RED
-                                + " has been warned for " + ChatColor.GRAY
-                                + warnreason + ChatColor.RED + ".",
-                        "rcmds.see.warn");
+                return true;
+            }
+            OfflinePlayer t2 = plugin.getServer().getOfflinePlayer(args[0].trim());
+            if (t2.isOp()) {
+                cs.sendMessage(ChatColor.RED + "You cannot warn that player!");
+                return true;
+            }
+            if (t2.isOnline()) {
+                if (plugin.isAuthorized((Player) t2, "rcmds.exempt.warn")) {
+                    cs.sendMessage(ChatColor.RED + "You cannot warn that player!");
+                    return true;
+                }
+            }
+            File pconfl = new File(plugin.getDataFolder() + "/userdata/" + t2.getName().toLowerCase() + ".yml");
+            if (pconfl.exists()) {
+                FileConfiguration pconf = YamlConfiguration.loadConfiguration(pconfl);
+                Integer numwarns;
+                String warnreason = null;
+                if (pconf.get("warns") == null) {
+                    numwarns = 0;
+                } else {
+                    numwarns = pconf.getConfigurationSection("warns").getValues(false).size();
+                }
+                if (numwarns == null) {
+                    numwarns = 0;
+                }
+                if (args.length == 1) {
+                    warnreason = plugin.defaultWarn;
+                    pconf.set("warns." + (numwarns + 1), warnreason);
+                    try {
+                        pconf.save(pconfl);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (args.length > 1) {
+                    warnreason = plugin.getFinalArg(args, 1);
+                    pconf.set("warns." + (numwarns + 1), warnreason);
+                    try {
+                        pconf.save(pconfl);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                cs.sendMessage(ChatColor.BLUE + "You have warned " + ChatColor.GRAY + t2.getName() + ChatColor.BLUE + ".");
+                if (t2.isOnline())
+                    ((Player) t2).sendMessage(ChatColor.RED + "You have been warned by " + ChatColor.GRAY + cs.getName() + ChatColor.RED + " for " + ChatColor.GRAY + warnreason + ChatColor.RED + ".");
+                plugin.getServer().broadcast(ChatColor.RED + "The player " + ChatColor.GRAY + t2.getName() + ChatColor.RED + " has been warned for " + ChatColor.GRAY + warnreason + ChatColor.RED + ".", "rcmds.see.warn");
                 if (plugin.warnBan > 0) {
                     if ((numwarns + 1) >= plugin.warnBan) {
                         t2.setBanned(true);
-                        if (t2.isOnline()) {
-                            ((Player) t2)
-                                    .kickPlayer(ChatColor.DARK_RED
-                                            + "You have been banned for reaching the max warn limit.");
-                        }
-                        plugin.getServer()
-                                .broadcast(
-                                        ChatColor.RED
-                                                + "The player "
-                                                + ChatColor.GRAY
-                                                + t2.getName()
-                                                + ChatColor.RED
-                                                + " has been banned for "
-                                                + ChatColor.DARK_RED
-                                                + "You have been banned for reaching the max warn limit."
-                                                + ChatColor.RED + ".",
-                                        "rcmds.see.ban");
+                        if (t2.isOnline())
+                            ((Player) t2).kickPlayer(ChatColor.DARK_RED + "You have been banned for reaching the max warn limit.");
+                        plugin.getServer().broadcast(ChatColor.RED + "The player " + ChatColor.GRAY + t2.getName() + ChatColor.RED + " has been banned for " + ChatColor.DARK_RED + "You have been banned for reaching the max warn limit." + ChatColor.RED + ".", "rcmds.see.ban");
                     }
                 }
                 return true;
