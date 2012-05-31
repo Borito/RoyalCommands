958319f42e4cb3268f879b4ee344c4ca35b67fb0
Kyle Clemens <jkc.clemens@gmail.com>
2012-05-30 23:13:27 -0400

    Fixed: /fc may work better (?)

--- src/org/royaldev/royalcommands/rcommands/CmdFixChunk.java
+++ src/org/royaldev/royalcommands/rcommands/CmdFixChunk.java
@@ -31,6 +31,8 @@ public class CmdFixChunk implements CommandExecutor {
             Player p = (Player) cs;
             Chunk c = p.getLocation().getChunk();
             boolean worked = p.getWorld().refreshChunk(c.getX(), c.getZ());
+            c.unload();
+            c.load();
             if (worked) cs.sendMessage(ChatColor.BLUE + "The chunk you're standing in has been reloaded!");
             else cs.sendMessage(ChatColor.RED + "The chunk could not be reloaded.");
             return true;
