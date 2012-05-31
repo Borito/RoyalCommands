3cdd6e8f58e3c9f2399c7b0962fe27339c4e3a34
Kyle Clemens <jkc.clemens@gmail.com>
2012-05-30 23:13:16 -0400

    Fixed: /nick now behaves much better!

--- src/org/royaldev/royalcommands/rcommands/CmdNick.java
+++ src/org/royaldev/royalcommands/rcommands/CmdNick.java
@@ -1,6 +1,7 @@
 package org.royaldev.royalcommands.rcommands;
 
 import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -21,85 +22,41 @@ public class CmdNick implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender cs, Command cmd, String label, String[] args) {
         if (cmd.getName().equalsIgnoreCase("nick")) {
-            if (!plugin.isAuthorized(cs, "rcmds.nick")) {
+            if (args.length < 2) {
+                cs.sendMessage(cmd.getDescription());
+                return false;
+            }
+            OfflinePlayer t = plugin.getServer().getOfflinePlayer(args[0]);
+            if (!t.equals(cs) && !plugin.isAuthorized(cs, "rcmds.others.nick")) {
                 RUtils.dispNoPerms(cs);
                 return true;
             }
-            if (args.length < 1) {
-                if (!(cs instanceof Player)) {
-                    cs.sendMessage(cmd.getDescription());
-                    return false;
-                }
-                Player p = (Player) cs;
-                String newname = p.getName();
-                p.setDisplayName(newname);
-                PConfManager.setPValString(p, newname, "dispname");
-                p.sendMessage(ChatColor.BLUE + "Your name has been reset to " + ChatColor.GRAY + newname + ChatColor.BLUE + ".");
+            if (!PConfManager.getPConfExists(t)) {
+                cs.sendMessage(ChatColor.RED + "That player doesn't exist!");
                 return true;
             }
-            if (args.length == 1) {
-                if (args[0].equalsIgnoreCase("help") || args[0].equals("?")) {
-                    cs.sendMessage(cmd.getDescription());
-                    return false;
+            if (args[1].equalsIgnoreCase("off")) {
+                PConfManager.setPValString(t, t.getName(), "dispname");
+                if (t.isOnline()) {
+                    Player p = (Player) t;
+                    p.setDisplayName(t.getName());
+                    p.setPlayerListName(t.getName());
+                    p.sendMessage(ChatColor.BLUE + "Your nickname was reset by " + ChatColor.GRAY + cs.getName() + ChatColor.BLUE + ".");
                 }
-                if (!(cs instanceof Player)) {
-                    cs.sendMessage(cmd.getDescription());
-                    return false;
-                }
-                Player p = (Player) cs;
-                String newname = plugin.nickPrefix + args[0];
-                if (newname.equalsIgnoreCase("none")) {
-                    p.setDisplayName(p.getName());
-                    p.setPlayerListName(p.getName());
-                    PConfManager.setPValString(p, p.getName(), "dispname");
-                    p.sendMessage(ChatColor.BLUE + "Removed your nickname.");
-                    return true;
-                }
-                p.setDisplayName(newname);
-                p.setPlayerListName(newname);
-                PConfManager.setPValString(p, newname, "dispname");
-                p.sendMessage(ChatColor.BLUE + "You have set your nickname to " + ChatColor.GRAY + newname + ChatColor.BLUE + ".");
+                cs.sendMessage(ChatColor.BLUE + "You reset the nickname of " + ChatColor.GRAY + t.getName() + ChatColor.BLUE + ".");
                 return true;
             }
-
-            if (args.length > 1) {
-                Player t = plugin.getServer().getPlayer(args[0]);
-                if ((cs instanceof Player) && t.equals(cs)) {
-                    Player p = (Player) cs;
-                    String newname = plugin.nickPrefix + args[0];
-                    if (newname.equalsIgnoreCase("none")) {
-                        p.setDisplayName(p.getName());
-                        p.setPlayerListName(p.getName());
-                        PConfManager.setPValString(p, p.getName(), "dispname");
-                        p.sendMessage(ChatColor.BLUE + "Removed your nickname.");
-                        return true;
-                    }
-                    p.setDisplayName(newname);
-                    p.setPlayerListName(newname);
-                    PConfManager.setPValString(p, newname, "dispname");
-                    p.sendMessage(ChatColor.BLUE + "You have set your nickname to " + ChatColor.GRAY + newname + ChatColor.BLUE + ".");
-                    return true;
-                }
-                if (!plugin.isAuthorized(cs, "rcmds.others.nick")) {
-                    RUtils.dispNoPerms(cs);
-                    return true;
-                }
-                if (t == null || plugin.isVanished(t)) {
-                    cs.sendMessage(ChatColor.RED + "That player does not exist!");
-                    return true;
-                }
-                if (plugin.isAuthorized(t, "rcmds.exempt.nick")) {
-                    cs.sendMessage(ChatColor.RED + "You cannot change that player's nick!");
-                    return true;
-                }
-                String newname = plugin.nickPrefix + args[0];
-                t.setDisplayName(newname);
-                t.setPlayerListName(newname);
-                PConfManager.setPValString(t, newname, "dispname");
-                t.sendMessage(ChatColor.BLUE + "Your display name has been changed to " + ChatColor.GRAY + newname + ChatColor.BLUE + ".");
-                cs.sendMessage(ChatColor.BLUE + "You have changed " + ChatColor.GRAY + t.getName() + "\'s" + ChatColor.BLUE + " name to " + ChatColor.GRAY + newname + ChatColor.BLUE + ".");
-                return true;
+            String newName = plugin.nickPrefix + args[1];
+            if (plugin.isAuthorized(cs, "rcmds.nick.color")) newName = RUtils.colorize(newName);
+            PConfManager.setPValString(t, newName, "dispname");
+            if (t.isOnline()) {
+                Player p = (Player) t;
+                p.setDisplayName(newName);
+                p.setPlayerListName(newName);
+                p.sendMessage(ChatColor.BLUE + "Your nickname was changed to " + ChatColor.GRAY + newName + ChatColor.BLUE + " by " + ChatColor.GRAY + cs.getName() + ChatColor.BLUE + ".");
             }
+            cs.sendMessage(ChatColor.BLUE + "Changed the nick of " + ChatColor.GRAY + t.getName() + ChatColor.BLUE + " to " + ChatColor.GRAY + newName + ChatColor.BLUE + ".");
+            return true;
         }
         return false;
     }
