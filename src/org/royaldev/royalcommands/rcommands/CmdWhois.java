ab96acb6aecd9d3dad8d571d21d951c003861cbb
Kyle Clemens <jkc.clemens@gmail.com>
2012-06-20 19:10:04 -0400

    Added: More info to /whois

--- src/org/royaldev/royalcommands/rcommands/CmdWhois.java
+++ src/org/royaldev/royalcommands/rcommands/CmdWhois.java
@@ -5,9 +5,13 @@ import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.royaldev.royalcommands.PConfManager;
 import org.royaldev.royalcommands.RUtils;
 import org.royaldev.royalcommands.RoyalCommands;
+
+import java.text.DecimalFormat;
+import java.util.Date;
 
 public class CmdWhois implements CommandExecutor {
 
@@ -32,17 +36,26 @@ public class CmdWhois implements CommandExecutor {
                 cs.sendMessage(ChatColor.RED + "That player has never played before!");
                 return true;
             }
+            DecimalFormat df = new DecimalFormat("#.##");
             String ip = PConfManager.getPValString(t, "ip");
-            String name = t.getName();
+            String name = PConfManager.getPValString(t, "name");
             String dispname = PConfManager.getPValString(t, "dispname");
             cs.sendMessage(ChatColor.BLUE + "=====================");
-            cs.sendMessage(ChatColor.BLUE + "Whois for " + ChatColor.GRAY + name);
+            cs.sendMessage(ChatColor.BLUE + ((t.isOnline()) ? "Whois" : "Whowas") + " for " + ChatColor.GRAY + name);
             cs.sendMessage(ChatColor.BLUE + "Nickname: " + ChatColor.GRAY + dispname);
             cs.sendMessage(ChatColor.BLUE + "IP: " + ChatColor.GRAY + ip);
             cs.sendMessage(ChatColor.BLUE + "Is VIP: " + ChatColor.GRAY + ((PConfManager.getPValBoolean(t, "vip")) ? "yes" : "no"));
             long timestamp = RUtils.getTimeStamp(t, "seen");
             String lastseen = (timestamp < 0) ? "unknown" : RUtils.formatDateDiff(timestamp);
-            cs.sendMessage(ChatColor.BLUE + "Last seen: " + ChatColor.GRAY + lastseen);
+            cs.sendMessage(ChatColor.BLUE + "Last seen:" + ChatColor.GRAY + ((t.isOnline()) ? " now" : lastseen));
+            cs.sendMessage(ChatColor.BLUE + "First played: " + ChatColor.GRAY + RUtils.formatDateDiff(t.getFirstPlayed()));
+            if (t.isOnline()) {
+                Player p = (Player) t;
+                cs.sendMessage(ChatColor.BLUE + "Health/Hunger/Saturation: " + ChatColor.GRAY + p.getHealth() / 2 + ChatColor.BLUE + "/" + ChatColor.GRAY + p.getFoodLevel() / 2 + ChatColor.BLUE + "/" + ChatColor.GRAY + p.getSaturation() / 2);
+                cs.sendMessage(ChatColor.BLUE + "Total Exp/Exp %/Level: " + ChatColor.GRAY + p.getTotalExperience() + ChatColor.BLUE + "/" + ChatColor.GRAY + df.format(p.getExp() * 100) + "%" + ChatColor.BLUE + "/" + ChatColor.GRAY + p.getLevel());
+                cs.sendMessage(ChatColor.BLUE + "Item in hand: " + ChatColor.GRAY + RUtils.getItemName(p.getItemInHand()));
+                cs.sendMessage(ChatColor.BLUE + "Alive for:" + ChatColor.GRAY + RUtils.formatDateDiff(new Date().getTime() - p.getTicksLived() * 50));
+            }
             cs.sendMessage(ChatColor.BLUE + "=====================");
             return true;
         }
