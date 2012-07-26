5df4113268a8481d5eb1e1ea03b67a28b60870d2
Kyle Clemens <jkc.clemens@gmail.com>
2012-07-26 00:16:24 -0400

    Fixed: NPE in /listhomes - rewritten

--- src/org/royaldev/royalcommands/rcommands/CmdListHome.java
+++ src/org/royaldev/royalcommands/rcommands/CmdListHome.java
@@ -5,14 +5,12 @@ import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.royaldev.royalcommands.PConfManager;
 import org.royaldev.royalcommands.RUtils;
 import org.royaldev.royalcommands.RoyalCommands;
 
-import java.io.File;
 import java.util.Map;
 
 public class CmdListHome implements CommandExecutor {
@@ -34,37 +32,34 @@ public class CmdListHome implements CommandExecutor {
                 cs.sendMessage(cmd.getDescription());
                 return false;
             }
-            File pconfl;
-            if (args.length < 1) {
-                pconfl = new File(plugin.getDataFolder() + File.separator + "userdata" + File.separator + cs.getName().toLowerCase() + ".yml");
-            } else {
-                if (!new PConfManager(args[0].trim().toLowerCase()).exists()) {
-                    cs.sendMessage(ChatColor.RED + "That player does not exist!");
-                    return true;
-                }
-                OfflinePlayer op = plugin.getServer().getOfflinePlayer(args[0].trim());
-                if (op.isOp() || (op.isOnline() && plugin.isAuthorized((Player) op, "rcmds.exempt.listhome"))) {
+            OfflinePlayer t;
+            if (args.length < 1) t = (OfflinePlayer) cs;
+            else {
+                t = plugin.getServer().getPlayer(args[0]);
+                if (t == null) t = plugin.getServer().getOfflinePlayer(args[0]);
+                if (plugin.isAuthorized(t, "rcmds.exempt.listhome")) {
                     cs.sendMessage(ChatColor.RED + "You can't list that player's homes!");
                     return true;
                 }
-                pconfl = new File(plugin.getDataFolder() + File.separator + "userdata" + File.separator + args[0].trim().toLowerCase() + ".yml");
             }
 
-            if (pconfl.exists()) {
-                FileConfiguration pconf = YamlConfiguration.loadConfiguration(pconfl);
-                final Map<String, Object> opts = pconf.getConfigurationSection("home").getValues(false);
-                if (opts.keySet().isEmpty()) {
-                    cs.sendMessage(ChatColor.RED + "No homes found!");
-                    return true;
-                }
-                String homes = opts.keySet().toString();
-                homes = homes.substring(1, homes.length() - 1);
-                cs.sendMessage(ChatColor.BLUE + "Homes:");
-                cs.sendMessage(homes);
+            PConfManager pcm = new PConfManager(t);
+            ConfigurationSection cfgs = pcm.getConfigurationSection("home");
+            if (cfgs == null) {
+                cs.sendMessage(ChatColor.RED + "No homes found!");
                 return true;
             }
+            final Map<String, Object> opts = cfgs.getValues(false);
+            if (opts.keySet().isEmpty()) {
+                cs.sendMessage(ChatColor.RED + "No homes found!");
+                return true;
+            }
+            String homes = opts.keySet().toString();
+            homes = homes.substring(1, homes.length() - 1);
+            cs.sendMessage(ChatColor.BLUE + "Homes:");
+            cs.sendMessage(homes);
+            return true;
         }
         return false;
-
     }
 }
