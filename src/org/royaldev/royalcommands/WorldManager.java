2766ad59e262be24af0e53e7491a8ab818f49cc0
Kyle Clemens <jkc.clemens@gmail.com>
2012-08-14 23:06:10 -0400

    Added: WorldManager and weather 'n stuff

--- src/org/royaldev/royalcommands/WorldManager.java
+++ src/org/royaldev/royalcommands/WorldManager.java
@@ -2,10 +2,14 @@ package org.royaldev.royalcommands;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Difficulty;
+import org.bukkit.GameMode;
 import org.bukkit.World;
 import org.bukkit.WorldCreator;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.weather.ThunderChangeEvent;
+import org.bukkit.event.weather.WeatherChangeEvent;
 import org.bukkit.event.world.WorldLoadEvent;
 import org.bukkit.event.world.WorldUnloadEvent;
 
@@ -19,6 +23,7 @@ public class WorldManager {
     private class WorldWatcher implements Listener {
         @EventHandler
         public void worldUnload(WorldUnloadEvent e) {
+            if (e.isCancelled()) return;
             synchronized (loadedWorlds) {
                 if (loadedWorlds.contains(e.getWorld().getName()))
                     loadedWorlds.remove(e.getWorld().getName());
@@ -32,6 +37,40 @@ public class WorldManager {
                     loadedWorlds.add(e.getWorld().getName());
                 addNewToConfig();
             }
+        }
+
+        @EventHandler
+        public void onWorldTele(PlayerChangedWorldEvent e) {
+            World to = e.getPlayer().getWorld();
+            if (!configuredWorlds.contains(to.getName())) return;
+            if (!loadedWorlds.contains(to.getName())) return;
+            GameMode gm;
+            try {
+                gm = GameMode.valueOf(config.getString("worlds." + to.getName() + ".gamemode", "SURVIVAL"));
+            } catch (IllegalArgumentException ex) {
+                gm = GameMode.SURVIVAL;
+            }
+            e.getPlayer().setGameMode(gm);
+        }
+
+        @EventHandler
+        public void onWeather(WeatherChangeEvent e) {
+            if (e.isCancelled()) return;
+            World w = e.getWorld();
+            if (!configuredWorlds.contains(w.getName())) return;
+            if (!loadedWorlds.contains(w.getName())) return;
+            boolean allowWeather = config.getBoolean("worlds." + w.getName() + ".weather", true);
+            if (!allowWeather) e.setCancelled(true);
+        }
+
+        @EventHandler
+        public void onThunder(ThunderChangeEvent e) {
+            if (e.isCancelled()) return;
+            World w = e.getWorld();
+            if (!configuredWorlds.contains(w.getName())) return;
+            if (!loadedWorlds.contains(w.getName())) return;
+            boolean allowWeather = config.getBoolean("worlds." + w.getName() + ".weather", true);
+            if (!allowWeather) e.setCancelled(true);
         }
     }
 
@@ -106,6 +145,29 @@ public class WorldManager {
         }
     }
 
+    public void addToLoadedWorlds(String name) {
+        synchronized (loadedWorlds) {
+            if (!loadedWorlds.contains(name)) loadedWorlds.add(name);
+        }
+    }
+
+    public void addToLoadedWorlds(World w) {
+        synchronized (loadedWorlds) {
+            if (!loadedWorlds.contains(w.getName())) loadedWorlds.add(w.getName());
+        }
+    }
+
+    public void removeFromLoadedWorlds(String name) {
+        synchronized (loadedWorlds) {
+            if (loadedWorlds.contains(name)) loadedWorlds.remove(name);
+        }
+    }
+
+    public void removeFromLoadedWorlds(World w) {
+        synchronized (loadedWorlds) {
+            if (loadedWorlds.contains(w.getName())) loadedWorlds.remove(w.getName());
+        }
+    }
 
     /**
      * Attempts to load a world.
@@ -125,8 +187,39 @@ public class WorldManager {
         if (!contains) throw new IllegalArgumentException("No such world!");
         WorldCreator wc = new WorldCreator(name);
         World w = wc.createWorld();
-        loadedWorlds.add(w.getName());
+        synchronized (loadedWorlds) {
+            loadedWorlds.add(w.getName());
+        }
         return w;
+    }
+
+    /**
+     * Attempts to unload a world
+     *
+     * @param w World to unload
+     * @return If world was unloaded
+     */
+    public boolean unloadWorld(World w) {
+        boolean worked = Bukkit.unloadWorld(w, true);
+        synchronized (loadedWorlds) {
+            if (loadedWorlds.contains(w.getName()) && worked)
+                loadedWorlds.remove(w.getName());
+        }
+        return worked;
+    }
+
+    /**
+     * Attempts to unload a world
+     *
+     * @param name Name of world to unload
+     * @return If world was unloaded
+     */
+    public boolean unloadWorld(String name) {
+        boolean worked = Bukkit.unloadWorld(name, true);
+        synchronized (loadedWorlds) {
+            if (loadedWorlds.contains(name) && worked) loadedWorlds.remove(name);
+        }
+        return worked;
     }
 
     public void addNewToConfig() {
@@ -141,6 +234,7 @@ public class WorldManager {
             config.setBoolean(w.getKeepSpawnInMemory(), path + "keepspawnloaded");
             config.setBoolean(w.canGenerateStructures(), path + "generatestructures");
             config.setBoolean(w.getPVP(), path + "pvp");
+            config.setBoolean(true, path + "weather");
             config.setInteger(w.getMaxHeight(), path + "maxheight");
             config.setInteger(w.getMonsterSpawnLimit(), path + "monsterspawnlimit");
             config.setInteger(w.getAnimalSpawnLimit(), path + "animalspawnlimit");
@@ -150,6 +244,7 @@ public class WorldManager {
             config.setString(w.getDifficulty().name(), path + "difficulty");
             config.setString(w.getWorldType().name(), path + "worldtype");
             config.setString(w.getEnvironment().name(), path + "environment");
+            config.setString(Bukkit.getServer().getDefaultGameMode().name(), path + "gamemode");
             config.setLong(w.getSeed(), path + "seed");
             config.setBoolean(false, path + "freezetime");
             config.setBoolean(true, path + "loadatstartup");
