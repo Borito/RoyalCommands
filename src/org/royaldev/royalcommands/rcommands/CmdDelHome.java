6afef16362061cd31fbf45d57e74c65d84563d41
Kyle Clemens <jkc.clemens@gmail.com>
2012-08-20 19:54:34 -0400

    Fixed: /delhome uses PConfManager

--- src/org/royaldev/royalcommands/rcommands/CmdDelHome.java
+++ src/org/royaldev/royalcommands/rcommands/CmdDelHome.java
@@ -4,14 +4,10 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+import org.royaldev.royalcommands.PConfManager;
 import org.royaldev.royalcommands.RUtils;
 import org.royaldev.royalcommands.RoyalCommands;
-
-import java.io.File;
-import java.io.IOException;
 
 public class CmdDelHome implements CommandExecutor {
 
@@ -32,26 +28,19 @@ public class CmdDelHome implements CommandExecutor {
                 cs.sendMessage(ChatColor.RED + "This command is only available to players!");
                 return true;
             }
+            Player p = (Player) cs;
             if (args.length < 1) {
                 cs.sendMessage(ChatColor.RED + "Type \"" + ChatColor.GRAY + "/delhome home" + ChatColor.RED + "\" to delete your default home.");
                 return true;
             }
-            File pconfl = new File(plugin.getDataFolder() + "/userdata/" + cs.getName().toLowerCase() + ".yml");
-            if (pconfl.exists()) {
-                FileConfiguration pconf = YamlConfiguration.loadConfiguration(pconfl);
-                if (pconf.get("home." + args[0]) == null) {
-                    cs.sendMessage(ChatColor.RED + "That home does not exist!");
-                    return true;
-                }
-                pconf.set("home." + args[0], null);
-                try {
-                    pconf.save(pconfl);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                cs.sendMessage(ChatColor.BLUE + "The home \"" + ChatColor.GRAY + args[0] + ChatColor.BLUE + "\" has been deleted.");
+            PConfManager pcm = new PConfManager(p);
+            if (pcm.get("home." + args[0]) == null) {
+                cs.sendMessage(ChatColor.RED + "That home does not exist!");
                 return true;
             }
+            pcm.set("home." + args[0], null);
+            cs.sendMessage(ChatColor.BLUE + "The home \"" + ChatColor.GRAY + args[0] + ChatColor.BLUE + "\" has been deleted.");
+            return true;
         }
         return false;
     }
