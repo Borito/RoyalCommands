988573b3f2cd1de0814f3cc5ea8e8a8d55590ace
Kyle Clemens <jkc.clemens@gmail.com>
2012-08-31 11:42:05 -0400

    Updated: /ping now displays ping milliseconds

--- src/org/royaldev/royalcommands/rcommands/CmdPing.java
+++ src/org/royaldev/royalcommands/rcommands/CmdPing.java
@@ -1,8 +1,11 @@
 package org.royaldev.royalcommands.rcommands;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.craftbukkit.entity.CraftPlayer;
+import org.bukkit.entity.Player;
 import org.royaldev.royalcommands.RUtils;
 import org.royaldev.royalcommands.RoyalCommands;
 
@@ -22,7 +25,28 @@ public class CmdPing implements CommandExecutor {
                 RUtils.dispNoPerms(cs);
                 return true;
             }
-            cs.sendMessage("Pong!");
+            if (!(cs instanceof Player) && args.length < 1) {
+                cs.sendMessage("Pong!");
+                return true;
+            }
+            if (args.length > 0) {
+                if (!plugin.isAuthorized(cs, "rcmds.others.ping")) {
+                    RUtils.dispNoPerms(cs);
+                    return true;
+                }
+                Player p = plugin.getServer().getPlayer(args[0]);
+                if (p == null || plugin.isVanished(p, cs)) {
+                    cs.sendMessage(ChatColor.RED + "That player does not exist!");
+                    return true;
+                }
+                int ping = ((CraftPlayer) p).getHandle().ping;
+                String possessive = (p.getName().endsWith("s")) ? "'" : "'s";
+                p.sendMessage(ChatColor.GRAY + p.getName() + possessive + ChatColor.BLUE + " ping: " + ChatColor.GRAY + ping + "ms");
+                return true;
+            }
+            Player p = (Player) cs;
+            int ping = ((CraftPlayer) p).getHandle().ping;
+            p.sendMessage(ChatColor.BLUE + "Your ping: " + ChatColor.GRAY + ping + "ms");
             return true;
         }
         return false;
