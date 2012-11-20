167666bb1650ea858fa44c5a08928d6a644e1006
Kyle Clemens <jkc.clemens@gmail.com>
2012-11-20 17:52:15 -0500

    Added: Enchantments to /getid (RCMDS-3 #resolve)

--- src/org/royaldev/royalcommands/rcommands/CmdGetID.java
+++ src/org/royaldev/royalcommands/rcommands/CmdGetID.java
@@ -4,10 +4,13 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.royaldev.royalcommands.RUtils;
 import org.royaldev.royalcommands.RoyalCommands;
+
+import java.util.Map;
 
 public class CmdGetID implements CommandExecutor {
 
@@ -34,7 +37,15 @@ public class CmdGetID implements CommandExecutor {
             short damage = hand.getDurability();
             byte data = hand.getData().getData();
             String name = RUtils.getItemName(hand);
+            Map<Enchantment, Integer> enchants = hand.getEnchantments();
             cs.sendMessage(ChatColor.GRAY + name + ChatColor.BLUE + ": " + ChatColor.GRAY + id + ChatColor.BLUE + " (damage: " + ChatColor.GRAY + damage + ChatColor.BLUE + ", materialdata: " + ChatColor.GRAY + data + ChatColor.BLUE + ")");
+            if (!enchants.isEmpty()) {
+                cs.sendMessage(ChatColor.BLUE + "Enchantments:");
+                for (Enchantment e : enchants.keySet()) {
+                    int lvl = enchants.get(e);
+                    cs.sendMessage(" " + ChatColor.GRAY + e.getName().toLowerCase() + " " + lvl);
+                }
+            }
             return true;
         }
         return false;
