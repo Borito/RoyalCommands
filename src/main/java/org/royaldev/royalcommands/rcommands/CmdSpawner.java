ffca6fa852a2ebba66a6b0871f80f5f20be8d869
Kyle Clemens <jkc.clemens@gmail.com>
2013-08-26 14:22:21 -0400

    Fixed: Exception in /spawner

--- src/main/java/org/royaldev/royalcommands/rcommands/CmdSpawner.java
+++ src/main/java/org/royaldev/royalcommands/rcommands/CmdSpawner.java
@@ -52,7 +52,7 @@ public class CmdSpawner implements CommandExecutor {
                 cs.sendMessage(MessageColor.NEGATIVE + "Invalid mob!");
                 return true;
             }
-            if (!plugin.ah.isAuthorized(cs, "rcmds.spawnmob." + ct.getName().toLowerCase()) && !plugin.ah.isAuthorized("cs", "rcmds.spawnmob.*")) {
+            if (!plugin.ah.isAuthorized(cs, "rcmds.spawnmob." + ct.getName().toLowerCase()) && !plugin.ah.isAuthorized(cs, "rcmds.spawnmob.*")) {
                 cs.sendMessage(MessageColor.NEGATIVE + "You cannot use mob type " + MessageColor.NEUTRAL + ct.getName().toLowerCase() + MessageColor.NEGATIVE + ".");
                 return true;
             }
