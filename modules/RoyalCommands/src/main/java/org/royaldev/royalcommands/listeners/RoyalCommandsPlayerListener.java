02d708ad60d7e1138780660249a8df2cdec8b930
Kyle Clemens <jkc.clemens@gmail.com>
2014-06-16 20:27:40 -0400

    Fixed: Only show people with command spy enabled

--- modules/RoyalCommands/src/main/java/org/royaldev/royalcommands/listeners/RoyalCommandsPlayerListener.java
+++ modules/RoyalCommands/src/main/java/org/royaldev/royalcommands/listeners/RoyalCommandsPlayerListener.java
@@ -189,6 +189,7 @@ public class RoyalCommandsPlayerListener implements Listener {
     public void commandSpy(PlayerCommandPreprocessEvent e) {
         if (plugin.ah.isAuthorized(e.getPlayer(), "rcmds.exempt.commandspy")) return;
         for (Player p : e.getPlayer().getServer().getOnlinePlayers()) {
+            if (!PConfManager.getPConfManager(p).getBoolean("commandspy", false)) continue;
             if (p.getName().equalsIgnoreCase(e.getPlayer().getName())) continue; // don't send to self
             p.sendMessage(MessageColor.NEUTRAL + e.getPlayer().getName() + MessageColor.POSITIVE + ": " + MessageColor.NEUTRAL + e.getMessage());
         }
