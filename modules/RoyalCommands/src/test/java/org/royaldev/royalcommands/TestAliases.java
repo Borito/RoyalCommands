e06471689606f282c4ccc01f1911f4eb4aef1d7e
Kyle Clemens <jkc.clemens@gmail.com>
2014-07-09 13:39:00 -0400

    Added: Test for reused aliases

--- modules/RoyalCommands/src/test/java/org/royaldev/royalcommands/TestAliases.java
+++ modules/RoyalCommands/src/test/java/org/royaldev/royalcommands/TestAliases.java
@@ -1,17 +1,33 @@
 package org.royaldev.royalcommands;
 
 import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.junit.Test;
 import org.royaldev.royalcommands.opencsv.CSVReader;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class TestAliases {
+
+    private YamlConfiguration pluginYml = null;
+
+    private YamlConfiguration getPluginYml() {
+        if (this.pluginYml == null) {
+            final File pluginFile = new File("src/main/resources/plugin.yml");
+            assertTrue("No plugin.yml found!", pluginFile.exists());
+            this.pluginYml = YamlConfiguration.loadConfiguration(pluginFile);
+        }
+        return this.pluginYml;
+    }
 
     @Test
     public void testAliases() throws Throwable {
@@ -27,6 +43,20 @@ public class TestAliases {
             l.warning("Missing alias for Material " + m.name() + ".");
         }
         assertTrue("Missing aliases!", allAliasesExist);
+    }
+
+    @Test
+    public void testCommandAliases() throws Throwable {
+        final ConfigurationSection reflectCommands = this.getPluginYml().getConfigurationSection("reflectcommands");
+        final List<String> usedAliases = new ArrayList<>();
+        for (String key : reflectCommands.getKeys(false)) {
+            final ConfigurationSection cs = reflectCommands.getConfigurationSection(key);
+            final List<String> aliases = cs.getStringList("aliases");
+            for (String alias : aliases) {
+                assertFalse("Alias " + alias + " is reused in command " + key + ".", usedAliases.contains(alias));
+            }
+            usedAliases.addAll(aliases);
+        }
     }
 
 }
