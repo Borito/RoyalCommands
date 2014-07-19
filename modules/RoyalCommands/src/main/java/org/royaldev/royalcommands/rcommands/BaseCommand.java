9310e6011680345c0c4b098e58dbffe895355b01
Kyle Clemens <jkc.clemens@gmail.com>
2014-07-19 03:09:41 -0400

    Fixed: Make Hastebin reports async

--- modules/RoyalCommands/src/main/java/org/royaldev/royalcommands/rcommands/BaseCommand.java
+++ modules/RoyalCommands/src/main/java/org/royaldev/royalcommands/rcommands/BaseCommand.java
@@ -69,67 +69,80 @@ public abstract class BaseCommand implements CommandExecutor {
         return "http://hastebin.com/" + hd.getKey() + ".txt";
     }
 
+    private void scheduleHastebin(final CommandSender cs, final String paste) {
+        this.plugin.getServer().getScheduler().runTaskAsynchronously(this.plugin, new Runnable() {
+            @Override
+            public void run() {
+                String tempURL;
+                try {
+                    tempURL = BaseCommand.this.hastebin(paste);
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                    tempURL = null;
+                }
+                final String url = tempURL;
+                BaseCommand.this.plugin.getServer().getScheduler().runTask(BaseCommand.this.plugin, new Runnable() {
+                    @Override
+                    public void run() {
+                        if (url != null) {
+                            BaseCommand.this.plugin.getLogger().warning("Error paste: " + url);
+                            // @formatter:off
+                            new FancyMessage("Click ")
+                                    .color(MessageColor.NEGATIVE._())
+                                .then("here")
+                                    .color(MessageColor.NEUTRAL._())
+                                    .tooltip("Click here to find out more.")
+                                    .link(url)
+                                .then(" to find out more.")
+                                    .color(MessageColor.NEGATIVE._())
+                                .send(cs);
+                            // @formatter:on
+                        } else {
+                            new FancyMessage(Config.hastebinErrors ? "An error occurred while trying to paste the stack trace." : "Error pasting is disabled.").color(MessageColor.NEGATIVE._()).send(cs);
+                        }
+                    }
+                });
+            }
+        });
+    }
+
     private void handleException(CommandSender cs, Command cmd, String label, String[] args, Throwable t) {
+        new FancyMessage("An exception occurred while processing that command.").color(MessageColor.NEGATIVE._()).send(cs);
         t.printStackTrace();
         final StringWriter sw = new StringWriter();
         t.printStackTrace(new PrintWriter(sw));
-        String url = null;
         if (Config.hastebinErrors) {
-            try {
-                final StringBuilder sb = new StringBuilder();
+            final StringBuilder sb = new StringBuilder();
+            sb
+                    // @formatter:off
+                    .append("An error occurred while handling a command. Please report this to jkcclemens or WizardCM.\n")
+                    .append("They are available at #bukkit @ irc.royaldev.org. If you don't know what that means, then\n")
+                    .append("go to the following URL: https://irc.royaldev.org/#bukkit\n\n")
+                    .append("---DEBUG INFO---\n\n");
+                    // @formatter:on
+            if (cs != null) {
                 sb
                         // @formatter:off
-                        .append("An error occurred while handling a command. Please report this to jkcclemens or WizardCM.\n")
-                        .append("They are available at #bukkit @ irc.royaldev.org. If you don't know what that means, then\n")
-                        .append("go to the following URL: https://irc.royaldev.org/#bukkit\n\n")
-                        .append("---DEBUG INFO---\n\n");
+                        .append("CommandSender\n")
+                        .append("\tName:\t\t").append(cs.getName()).append("\n")
+                        .append("\tClass:\t\t").append(cs.getClass().getName());
                         // @formatter:on
-                if (cs != null) {
-                    sb
-                            // @formatter:off
-                            .append("CommandSender\n")
-                            .append("\tName:\t\t").append(cs.getName()).append("\n")
-                            .append("\tClass:\t\t").append(cs.getClass().getName());
-                            // @formatter:on
-                } else sb.append("CommandSender:\t\tnull");
-                if (cmd != null) {
-                    sb
-                            // @formatter:off
-                            .append("\n\nCommand\n")
-                            .append("\tName:\t\t").append(cmd.getName()).append("\n")
-                            .append("\tClass:\t\t").append(cmd.getClass().getName());
-                            // @formatter:on
-                } else sb.append("\n\nCommand:\t\tnull");
-                sb.append("\n\nLabel:\t\t").append(label);
-                sb.append("\n\nArguments:");
-                if (args != null) {
-                    for (final String arg : args) sb.append("\n").append("\t").append(arg);
-                } else sb.append("\t\tnull");
-                sb.append("\n\n---STRACK TRACE---\n\n").append(sw.toString());
-                url = this.hastebin(sb.toString());
-            } catch (IOException ex) {
-                ex.printStackTrace();
-                url = null;
-            }
-        }
-        // @formatter:off
-        new FancyMessage("An exception occurred while processing that command.")
-                .color(MessageColor.NEGATIVE._())
-            .send(cs);
-        if (url != null) {
-            this.plugin.getLogger().warning("Error paste: " + url);
-            new FancyMessage("Click ")
-                    .color(MessageColor.NEGATIVE._())
-                .then("here")
-                    .color(MessageColor.NEUTRAL._())
-                    .tooltip("Click here to find out more.")
-                    .link(url)
-                .then(" to find out more.")
-                    .color(MessageColor.NEGATIVE._())
-                .send(cs);
-        // @formatter:on
-        } else {
-            new FancyMessage(Config.hastebinErrors ? "An error occurred while trying to paste the stack trace." : "Error pasting is disabled.").color(MessageColor.NEGATIVE._()).send(cs);
+            } else sb.append("CommandSender:\t\tnull");
+            if (cmd != null) {
+                sb
+                        // @formatter:off
+                        .append("\n\nCommand\n")
+                        .append("\tName:\t\t").append(cmd.getName()).append("\n")
+                        .append("\tClass:\t\t").append(cmd.getClass().getName());
+                        // @formatter:on
+            } else sb.append("\n\nCommand:\t\tnull");
+            sb.append("\n\nLabel:\t\t").append(label);
+            sb.append("\n\nArguments");
+            if (args != null) {
+                for (final String arg : args) sb.append("\n").append("\t").append(arg);
+            } else sb.append("\t\tnull");
+            sb.append("\n\n---STRACK TRACE---\n\n").append(sw.toString());
+            this.scheduleHastebin(cs, sb.toString());
         }
     }
 
