8c7730fe91561176b33855123f9183c54dc40f28
Kyle Clemens <jkc.clemens@gmail.com>
2014-12-07 00:26:09 -0500

    Updated: Separate homes by world

--- modules/RoyalCommands/src/main/java/org/royaldev/royalcommands/rcommands/CmdListHomes.java
+++ modules/RoyalCommands/src/main/java/org/royaldev/royalcommands/rcommands/CmdListHomes.java
@@ -1,6 +1,7 @@
 package org.royaldev.royalcommands.rcommands;
 
 import org.bukkit.OfflinePlayer;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -12,7 +13,11 @@ import org.royaldev.royalcommands.rcommands.home.Home;
 import org.royaldev.royalcommands.shaded.mkremins.fanciful.FancyMessage;
 import org.royaldev.royalcommands.wrappers.RPlayer;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 @ReflectCommand
 public class CmdListHomes extends BaseCommand {
@@ -41,16 +46,26 @@ public class CmdListHomes extends BaseCommand {
             }
         }
         final RPlayer rp = RPlayer.getRPlayer(t);
-        final Iterator<Home> homes = rp.getHomes().iterator();
-        final FancyMessage fm = new FancyMessage("");
-        while (homes.hasNext()) {
-            final Home home = homes.next();
-            fm.then(home.getName()).color(MessageColor.NEUTRAL._()).command("/home " + home.getFullName());
-            if (homes.hasNext()) fm.then(MessageColor.RESET + ", "); // it's not a color OR a style
+        final Map<World, List<Home>> sortedHomes = new HashMap<>();
+        for (final Home h : rp.getHomes()) {
+            final World w = h.getLocation().getWorld();
+            final List<Home> homes = sortedHomes.containsKey(w) ? sortedHomes.get(w) : new ArrayList<Home>();
+            homes.add(h);
+            sortedHomes.put(h.getLocation().getWorld(), homes);
         }
         final int homeLimit = rp.getHomeLimit();
         cs.sendMessage(MessageColor.POSITIVE + "Homes (" + MessageColor.NEUTRAL + rp.getHomes().size() + MessageColor.POSITIVE + "/" + MessageColor.NEUTRAL + ((homeLimit < 0) ? "Unlimited" : homeLimit) + MessageColor.POSITIVE + "):");
-        fm.send(cs);
+        for (final Map.Entry<World, List<Home>> entry : sortedHomes.entrySet()) {
+            cs.sendMessage(MessageColor.POSITIVE + "Homes for " + MessageColor.NEUTRAL + entry.getKey().getName() + MessageColor.POSITIVE + ":");
+            final FancyMessage fm = new FancyMessage("");
+            final Iterator<Home> homes = entry.getValue().iterator();
+            while (homes.hasNext()) {
+                final Home home = homes.next();
+                fm.then(home.getName()).color(MessageColor.NEUTRAL._()).command("/home " + home.getFullName());
+                if (homes.hasNext()) fm.then(MessageColor.RESET + ", "); // it's not a color OR a style
+            }
+            fm.send(cs);
+        }
         return true;
     }
 }
