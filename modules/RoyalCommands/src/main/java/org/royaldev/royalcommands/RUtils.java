e069ad0e7638b0fb5dcca298f96ee3eb7e959283
Matt Gajownik <matt@wizardcm.com>
2018-11-11 12:33:45 +1100

    Updated: Don't show seconds in "time since" util

--- modules/RoyalCommands/src/main/java/org/royaldev/royalcommands/RUtils.java
+++ modules/RoyalCommands/src/main/java/org/royaldev/royalcommands/RUtils.java
@@ -376,8 +376,8 @@ public final class RUtils {
         if (toDate.equals(fromDate)) return " now";
         if (toDate.after(fromDate)) future = true;
         StringBuilder sb = new StringBuilder();
-        int[] types = new int[]{Calendar.YEAR, Calendar.MONTH, Calendar.DAY_OF_MONTH, Calendar.HOUR_OF_DAY, Calendar.MINUTE, Calendar.SECOND};
-        String[] names = new String[]{"year", "years", "month", "months", "day", "days", "hour", "hours", "minute", "minutes", "second", "seconds"};
+        int[] types = new int[]{Calendar.YEAR, Calendar.MONTH, Calendar.DAY_OF_MONTH, Calendar.HOUR_OF_DAY, Calendar.MINUTE};
+        String[] names = new String[]{"year", "years", "month", "months", "day", "days", "hour", "hours", "minute", "minutes"};
         for (int i = 0; i < types.length; i++) {
             int diff = dateDiff(types[i], fromDate, toDate, future);
             if (diff > 0) sb.append(" ").append(diff).append(" ").append(names[i * 2 + (diff > 1 ? 1 : 0)]);
